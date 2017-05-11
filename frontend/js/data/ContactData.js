cc8e09796f9940d5615d8ddc416379f81445349d
Indra Eka Prasetya <indraep92@gmail.com>
2017-05-11 11:15:17 +0700

    add contact

---
+++ frontend/js/data/ContactData.js
@@ -0,0 +1,5 @@
+export default {
+    'linkedin_url': 'https://www.linkedin.com/in/indraep/',
+    'github_url': 'https://github.com/indraep',
+    'email_url': 'indraep92@gmail.com'
+};
--- frontend/js/pages/Contact.js
+++ frontend/js/pages/Contact.js
@@ -2,11 +2,33 @@ import React from 'react';
 
 import { Element } from 'react-scroll';
 
+import ContactData from '../data/ContactData';
+
+import LinkedinImg from '../../img/linkedin.png';
+import GithubImg from '../../img/github.png';
+import MailImg from '../../img/email.png';
+
 class Contact extends React.Component {
 	render() {
 		return (
 			<Element name="contact" className="container">
-				Contact
+				<div className="w16 content">
+					<div className="content__text">
+						<span>Find me in :</span>
+
+						<span className="contact">
+							<a href={ContactData.linkedin_url} target='_blank'>
+								<img src={LinkedinImg} />
+							</a>
+							<a href={`mailto:${ContactData.email_url}`} target='_blank'>
+								<img src={MailImg} />
+							</a>
+							<a href={ContactData.github_url} target='_blank'>
+								<img src={GithubImg} />
+							</a>
+						</span>
+					</div>
+				</div>
 			</Element>
 		);
 	}
