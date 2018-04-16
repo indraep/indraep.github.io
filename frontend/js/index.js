e6bb93446cd55bcbe112d55d4f76212f5837661b
GDN\indra.e.prasetya <indra.e.prasetya@gdn-commerce.com>
2018-04-16 16:03:48 +0700

    change

--- frontend/js/index.js
+++
@@ -1,7 +0,0 @@
-import '../css/style.scss';
-
-import React from 'react';
-import { render } from 'react-dom';
-import Root from './routes';
-
-render(<Root />, document.getElementById('container'));
